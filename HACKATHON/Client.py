b0b547706cae746e0d6a16f9ce87b89e66a2371b
AdvaHalamish <168714739+AdvaHalamish@users.noreply.github.com>
2025-01-15 11:56:08 +0200

    Update Client.py

--- HACKATHON/Client.py
+++ HACKATHON/Client.py
@@ -14,6 +14,9 @@ class SpeedTestClient:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         self.sock.bind(('', 13117))
         self.server = None
         self.active = True
